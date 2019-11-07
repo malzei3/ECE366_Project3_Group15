dc7b91a25daaa0fa68032a3a1033fc08c3a573f4
Metal079 <Metal145965@gmail.com>
2019-11-07 00:29:34 -0600

    Fixed a bug where an extra 2 would be added

--- prog.asm
+++ prog.asm
@@ -1,10 +1,9 @@
-init r2, 0 #r2 is set to 0		PC = 0
 subi r2, 3
 subi r2, 3 #r2 is now 250(0xFA) since by subtracting 6 from 0 it underflows.
 
-init r0, 4 # Sets r0 to 4 (Keeps track of where in the loop we are) (We will do a seperate loop for values 1 - 5)
+init r0, 4 # Sets r0 to 4 (Keeps track of where in the loop we are)
 
-init r1, 1 # Sets r1 to 4 (This will store the hash result)
+init r1, 1 # Sets r1 to 1 (This will store the hash result)
 
 hashing:
 hash r1, r2 # Hashes r1 and r2, r1 is set as result.
@@ -16,42 +15,35 @@ addi r0, 1 	# r0 increments
 cpy r1, r0	# r1 is set equal to r0
 sub r1, 3
 
-bnezR0 hashing	# When r0 != 0 the loop will end
+bnezR0 hashing	# When r0 == 0 the loop will end
 
-#We still need to store Hash results in mem[256] - mem[260]
+#We still need to store Hash results in mem[256] - mem[259]
 
 init r0, 1
-init r1, 0
+subi r0, 3
+subi r0, 1 # r0 = 253
+init r1, 1
 subi r1, 3
-subi r1, 1 # r1 = 251
+subi r1, 1 # r1 = 253
 
 hash r1, r2
 minc r1
 
-saal r1, r0
-addi r0, 1 	# r0 increments 
-cpy r1, r0	# r1 is set equal to r0
-subi r1, 3
-subi r1, 1 # r1 = 251
-
-hash r1, r2
-minc r1
-
-saal r1, r0
+saal r1, 1
 addi r0, 1 	# r0 increments 
 cpy r1, r0	# r1 is set equal to r0
 
 hash r1, r2
 minc r1
 
-saal r1, r0
+saal r1, 2
 addi r0, 1 	# r0 increments 
 cpy r1, r0	# r1 is set equal to r0
 
 hash r1, r2
 minc r1
 
-saal r1, r0
+saal r1, 3
 addi r0, 1 	# r0 increments 
 cpy r1, r0	# r1 is set equal to r0
 
